6b60753620d857c21ae66a13eb1adb1036614213
P1R47E <yosephtesfaye27@gmail.com>
2025-01-17 22:49:28 +0300

    Auth App

--- beaconproject/asgi.py
+++ beaconproject/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for authenticationjwt project.
+ASGI config for beaconproject project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'authenticationjwt.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'beaconproject.settings')
 
 application = get_asgi_application()
--- beaconproject/urls.py
+++ beaconproject/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for authenticationjwt project.
+URL configuration for beaconproject project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
