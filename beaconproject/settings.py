21adc70d18709ebf06d6bc158bdbe665911de0fc
P1R47E <yosephtesfaye27@gmail.com>
2025-01-18 11:09:24 +0300

    setting the setting

--- beaconproject/settings.py
+++ beaconproject/settings.py
@@ -1,7 +1,20 @@
+from pathlib import Path
+from datetime import timedelta
+import os
+
 INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    #third party apps
     
     'rest_framework',
     'corsheaders',
+    
+    # local apps
     'authApp',
 ]
 
@@ -26,9 +39,31 @@ AUTH_USER_MODEL = 'authApp.BeaconUser'
 
 MIDDLEWARE = [
     
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
    
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 
